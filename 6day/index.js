0e21ecfee4e30448d233cb8ebe64ff5e8843823f
Gergo Schreiner <gergo.schreiner@logmein.com>
2022-12-11 22:40:17 +0100

    Day 6. easy.

---
+++ 6day/index.js
@@ -0,0 +1,6 @@
+'use strict'; // use ES6 (ES2015 - ECMAScript 2015) standards
+
+// let - declare variables - ES6 (ES2015)
+let filePath = 'inputs.txt'
+let reader = new FileReader(filePath);
+console.log(reader.result);
---
+++ 6day/main.py
@@ -0,0 +1,27 @@
+def load_input():
+    with open('inputs.txt', mode='r') as file:
+        content = file.read()
+        return content
+
+
+def eval(signal):
+    block_size = 14
+    counter = 0
+    signal_length = len(signal)
+
+    for bit in range(signal_length):
+        sector = signal[bit:block_size + bit]
+        set_of_sec = set(sector)
+        if len(set_of_sec) == block_size:
+            break
+        counter += 1
+
+    return counter + block_size
+
+
+def main():
+    print(eval(load_input()))
+
+
+if __name__ == '__main__':
+    main()
